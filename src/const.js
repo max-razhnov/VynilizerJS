9ec26e11c14d67d645e2445136ff14e14ac04caf
max razhnov <shahteryshka@gmail.com>
2019-08-25 17:44:12 +0300

    add: own file for constants

---
+++ src/const.js
@@ -0,0 +1,17 @@
+export const buttonPlay = document.getElementsByClassName("btn")[1];
+export const buttonNext = document.getElementById("next");
+export const buttonPrevious = document.getElementById("previous");
+export const plst = document.getElementsByClassName("codek")[0];
+export const audio = document.getElementById("audio");
+export const nameOfSong = document.getElementById("song-name");
+export const timer = document.getElementById("info");
+export const audioSongs = [
+  "Adrift",
+  "A_Great_Darkness_Approaches_Can_You_Feel_It",
+  "Apollo_The_Wicked"
+];
+export const format = ".mp3";
+export const btnSongTop = document.getElementById("0");
+export const btnSongMiddle = document.getElementById("1");
+export const btnSongDown = document.getElementById("2");
+export const strela = document.getElementById("strela");
